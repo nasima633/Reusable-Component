cd9eeee122d3d94429e929b126d5889ae0fa81fd
Nasima Alizada <nasimaalizad12@gmail.com>
2026-01-15 09:53:06 +0430

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import UserInfo from './UserInfo'
+
+function App() {
+ return(
+  <div className='text'>
+   <UserInfo name="Nasima" lastname="Alizada" id="001"></UserInfo>
+    <UserInfo name="Nazia" lastname="Mihrabi" id="002"></UserInfo>
+     <UserInfo name="Sahar" lastname="Mihrabi" id="003"></UserInfo>
+      <UserInfo name="Mursal" lastname="Ahmadi" id="004"></UserInfo>
+  </div>
+ )
+}
+
+export default App
---
+++ src/UserInfo.jsx
@@ -0,0 +1,7 @@
+export default function UserInfo({name , lastname,id}){
+return(
+    
+    <p>name:{name} <br />lastname:{lastname} <br />ID:{id}</p>
+)
+
+}
